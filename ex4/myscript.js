df5b9ee0a37be8d6a3a9430f42105cb2937dfd7a
webarata3 <webarata3@gmail.com>
2016-01-07 04:54:46 +0900

    ex4のコミット

---
+++ ex4/myscript.js
@@ -0,0 +1,29 @@
+var colors = document.getElementsByName('colors');
+
+document.getElementById('save').onclick = function() {
+  if (colors[0].checked) {
+    localStorage['color'] = 'red';
+  } else if (colors[1].checked) {
+    localStorage['color'] = 'green';
+  } else {
+    localStorage['color'] = 'blue';
+  }
+};
+
+document.body.onload = function() {
+  console.log(localStorage['color']);
+  switch(localStorage['color']) {
+    case 'red':
+      colors[0].checked = true;
+      break;
+    case 'green':
+      colors[1].checked = true;
+      break;
+    case 'blue':
+      colors[2].checked = true;
+      break;
+    default:
+      colors[0].checked = true;
+      break;
+  }
+};
