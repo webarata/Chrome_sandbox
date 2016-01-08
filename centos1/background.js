457af8ef53105ce3282b70aad78dec8d5dc6ff66
webarata3 <webarata3@gmail.com>
2016-01-09 03:34:36 +0900

    CnetOS test1にバッジの追加

--- centos1/background.js
+++ centos1/background.js
@@ -1,12 +1,26 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(data) {
-  if (xhr.readyState == 4) {
-    if (xhr.status == 200) {
-      var contents = xhr.responseText;
-      console.log(contents);
+var ajax = function(url, doneCallBack, failCallBack) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(data) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        doneCallBack(xhr.responseText);
+      } else {
+        failCallBack(xhr.statusText)
+      }
     }
-  }
+  };
+  xhr.onerror = function(e) {
+    failCallback(xhr.statusText);
+  };
+
+  xhr.open('GET', url, true);
+  xhr.send();
 };
-var url = 'https://lists.centos.org/pipermail/centos-announce/';
-xhr.open('GET', url, true);
-xhr.send();
+
+ajax('https://lists.centos.org/pipermail/centos-announce/', function(responseText) {
+  chrome.browserAction.setBadgeText({ text: '1111' });
+  console.log(responseText);
+}, function(statusText) {
+  chrome.browserAction.setBadgeText({ text: '?' });
+});
+
