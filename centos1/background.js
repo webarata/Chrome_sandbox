f0d3eb5c3f12baceb98cba6631174b888d8831f0
webarata3 <webarata3@gmail.com>
2016-01-09 11:08:30 +0900

    CentOS test1のデータ取得を関数に変更

--- centos1/background.js
+++ centos1/background.js
@@ -17,20 +17,23 @@ var ajax = function(url, doneCallBack, failCallBack) {
   xhr.send();
 };
 
-ajax(url, function(responseText) {
-  var count = responseText.length;
-  var criticalCount =  (count - responseText.replace(/Critical/g, '').length) / 'Critical'.length;
-  var importantCount =  (count - responseText.replace(/Important/g, '').length) / 'Important'.length;
-  var moderateCount =  (count - responseText.replace(/Moderate/g, '').length) / 'Moderate'.length;
+var getData = function() {
+  ajax(url, function(responseText) {
+    var count = responseText.length;
+    var criticalCount = (count - responseText.replace(/Critical/g, '').length) / 'Critical'.length;
+    var importantCount = (count - responseText.replace(/Important/g, '').length) / 'Important'.length;
+    var moderateCount = (count - responseText.replace(/Moderate/g, '').length) / 'Moderate'.length;
 
-  localStorage['criticalCount'] = criticalCount;
-  localStorage['importantCount'] = importantCount;
-  localStorage['moderateCount'] = moderateCount;
+    localStorage['criticalCount'] = criticalCount;
+    localStorage['importantCount'] = importantCount;
+    localStorage['moderateCount'] = moderateCount;
 
-  chrome.browserAction.setBadgeText({ text:  (criticalCount + importantCount).toString() });
-  chrome.browserAction.setBadgeBackgroundColor({ color:[255, 0, 0, 0] });
-}, function(statusText) {
-  chrome.browserAction.setBadgeText({ text: '-' });
-  chrome.browserAction.setBadgeBackgroundColor({ color:[0, 0, 255, 100] });
-});
+    chrome.browserAction.setBadgeText({text: (criticalCount + importantCount).toString()});
+    chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 0]});
+  }, function(statusText) {
+    chrome.browserAction.setBadgeText({text: '-'});
+    chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 255, 100]});
+  });
+};
 
+getData();
