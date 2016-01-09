d69b3b38a50c37643747148b0a5d7494a8652eb0
webarata3 <webarata3@gmail.com>
2016-01-09 12:56:43 +0900

    CentOS test1の以前と件数が変わった場合にバッジの色を変える

--- centos1/background.js
+++ centos1/background.js
@@ -17,6 +17,22 @@ var ajax = function(url, doneCallBack, failCallBack) {
   xhr.send();
 };
 
+var getCounts = function() {
+  var criticalCount = localStorage['criticalCount'] || 0;
+  var importantCount = localStorage['importantCount'] || 0;
+  var moderateCount = localStorage['moderateCount'] || 0;
+  return [criticalCount, importantCount, moderateCount];
+};
+
+var compare = function(counts1, counts2) {
+  for (var i = 0; i < counts1.length; i++) {
+    if (counts1[i] != counts2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 var getData = function() {
   ajax(url, function(responseText) {
     var count = responseText.length;
@@ -24,15 +40,20 @@ var getData = function() {
     var importantCount = (count - responseText.replace(/Important/g, '').length) / 'Important'.length;
     var moderateCount = (count - responseText.replace(/Moderate/g, '').length) / 'Moderate'.length;
 
+    if (compare(getCounts(), [criticalCount, importantCount, moderateCount])) {
+      chrome.browserAction.setBadgeBackgroundColor({color: '#0000ff'});
+    } else {
+      chrome.browserAction.setBadgeBackgroundColor({color: '#ff0000'});
+    }
+
     localStorage['criticalCount'] = criticalCount;
     localStorage['importantCount'] = importantCount;
     localStorage['moderateCount'] = moderateCount;
 
     chrome.browserAction.setBadgeText({text: (criticalCount + importantCount).toString()});
-    chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 0]});
   }, function(statusText) {
     chrome.browserAction.setBadgeText({text: '-'});
-    chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 255, 100]});
+    chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 100]});
   });
 };
 
