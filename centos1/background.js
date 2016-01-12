0797e432c9605e72fd9f6550e5f8a50d3f306f8c
webarata3 <webarata3@gmail.com>
2016-01-12 16:21:56 +0900

    .jshintrcを追加

--- centos1/background.js
+++ centos1/background.js
@@ -17,16 +17,52 @@ var ajax = function(url, doneCallBack, failCallBack) {
   xhr.send();
 };
 
-var getCounts = function() {
+var getStatus = function(responseText) {
+  var count = responseText.length;
+  var criticalCount = (count - responseText.replace(/Critical/g, '').length) / 'Critical'.length;
+  var importantCount = (count - responseText.replace(/Important/g, '').length) / 'Important'.length;
+  var moderateCount = (count - responseText.replace(/Moderate/g, '').length) / 'Moderate'.length;
+
+  var date = new Date();
+  var month = date.getMonth() + 1;
+  var lastUpdateMonth = date.getFullYear().toString() + (month < 10 ? '0' + month : month);
+
+  return {
+    counts: [criticalCount, importantCount, moderateCount],
+    lastUpdateMonth: lastUpdateMonth
+  };
+};
+
+var setLocalStrage = function(status) {
+  localStorage['criticalCount'] = status.counts[0];
+  localStorage['importantCount'] = status.counts[1];
+  localStorage['moderateCount'] = status.counts[2];
+  localStorage['lastUpdateMonth'] = status.lastUpdateMonth;
+};
+
+var getSavedStatus = function() {
   var criticalCount = localStorage['criticalCount'] || 0;
   var importantCount = localStorage['importantCount'] || 0;
   var moderateCount = localStorage['moderateCount'] || 0;
-  return [criticalCount, importantCount, moderateCount];
+  var lastUpdateMonth = localStorage['lastUpdateMonth'] || '0';
+
+  return {
+    counts: [criticalCount, importantCount, moderateCount],
+    lastUpdateMonth: lastUpdateMonth
+  };
 };
 
-var compare = function(counts1, counts2) {
-  for (var i = 0; i < counts1.length; i++) {
-    if (counts1[i] != counts2[i]) {
+var equalStatus = function(status1, status2) {
+  if (status1.lastUpdateMonth != status2.lastUpdateMonth) {
+    for (var i = 0; i < status2.counts.length; i++) {
+      if (status2.counts[i] > 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  for (var i = 0; i < status1.counts.length; i++) {
+    if (status1.counts[i] != status2.counts[i]) {
       return false;
     }
   }
@@ -35,22 +71,17 @@ var compare = function(counts1, counts2) {
 
 var getData = function() {
   ajax(url, function(responseText) {
-    var count = responseText.length;
-    var criticalCount = (count - responseText.replace(/Critical/g, '').length) / 'Critical'.length;
-    var importantCount = (count - responseText.replace(/Important/g, '').length) / 'Important'.length;
-    var moderateCount = (count - responseText.replace(/Moderate/g, '').length) / 'Moderate'.length;
+    var status = getStatus(responseText);
 
-    if (compare(getCounts(), [criticalCount, importantCount, moderateCount])) {
+    if (equalStatus(getSavedStatus(), status)) {
       chrome.browserAction.setBadgeBackgroundColor({color: '#0000ff'});
     } else {
       chrome.browserAction.setBadgeBackgroundColor({color: '#ff0000'});
     }
 
-    localStorage['criticalCount'] = criticalCount;
-    localStorage['importantCount'] = importantCount;
-    localStorage['moderateCount'] = moderateCount;
+    setLocalStrage(status);
 
-    chrome.browserAction.setBadgeText({text: (criticalCount + importantCount).toString()});
+    chrome.browserAction.setBadgeText({text: (status.counts[0] + status.counts[1]).toString()});
   }, function(statusText) {
     chrome.browserAction.setBadgeText({text: '-'});
     chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 100]});
